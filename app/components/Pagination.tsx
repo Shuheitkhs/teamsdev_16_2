d82f8cd06b940f830e2821b050c15ffe1dfc4cfa
Stk <shuheitakahashi0822@gmail.com>
2024-10-14 21:35:01 +0900

    ✨ ページネーションの作成

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -5,33 +5,50 @@ import PaginationItem from "@mui/material/PaginationItem";
 import Stack from "@mui/material/Stack";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { useEffect, useState } from "react";
 
 export default function CustomIcons() {
+  const [isWideScreen, setIsWideScreen] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsWideScreen(window.innerWidth > 640);
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <Stack spacing={15}>
-      <Pagination
-        count={10}
-        renderItem={(item) => (
-          <PaginationItem
-            {...item}
-            components={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
-            slots={{
-              previous: ({ ...props }) => (
-                <div className="flex items-center">
-                  <ArrowBackIcon />
-                  <span>Previous Page</span>
-                </div>
-              ),
-              next: ({ ...props }) => (
-                <div className="flex items-center">
-                  <span>Next Page</span>
-                  <ArrowForwardIcon />
-                </div>
-              ),
-            }}
-          />
-        )}
-      />
-    </Stack>
+    <div className="flex justify-center">
+      <Stack spacing={3}>
+        <Pagination
+          count={10}
+          renderItem={(item) => (
+            <PaginationItem
+              {...item}
+              components={{ previous: ArrowBackIcon, next: ArrowForwardIcon }}
+              slots={{
+                previous: ({ ...props }) => (
+                  <div className="flex items-center">
+                    <ArrowBackIcon />
+                    {isWideScreen && <span>Previous Page</span>}
+                  </div>
+                ),
+                next: ({ ...props }) => (
+                  <div className="flex items-center">
+                    {isWideScreen && <span>Next Page</span>}
+                    <ArrowForwardIcon />
+                  </div>
+                ),
+              }}
+            />
+          )}
+        />
+      </Stack>
+    </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ import Pagination from "@/app/components/Pagination";
 export default function Home() {
   return (
     <>
-      <p className="text-red-500 text-bold">Hello world!!</p>
-      <Pagination />
+      <p>Hello world!!</p>
     </>
   );
 }
