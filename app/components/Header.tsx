70ff519bd2fbe8130c7f4340950fb3dd38ba030b
reoroi <reo0330210@gmail.com>
2024-10-15 10:27:11 +0900

    Headerコンポーネント枠組み作成

---
+++ app/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <p>LOGO</p>
+    <button>Home</button>
+    <button>Create</button>
+    <button>Sign In</button>
+    </>
+  )
+}
+
+export default Header
