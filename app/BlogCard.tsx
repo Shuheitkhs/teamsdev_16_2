378dbb4cce456742499ec495a099e0de3b1e4d7c
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2024-10-15 11:33:30 +0900

    ブログカードコンポーネント作成

---
+++ app/BlogCard.tsx
@@ -0,0 +1,18 @@
+export default function BlogCard() {
+    return (
+        <div>
+            <div>
+                <h3>Blog Title</h3>
+            </div>
+            <div>
+                <div>Profile Picture</div>
+            </div>
+            <div>
+                <div>Thumbnail</div>
+            </div>
+            <div>
+                <p>Contents</p>
+            </div>
+        </div>
+    )
+}
