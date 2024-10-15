b70d82743144728e003e6f97e49de97739727441
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2024-10-15 11:24:46 +0900

    homeコンポーネント作成

---
+++ app/home/page.tsx
@@ -0,0 +1,27 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+        <header>
+            <div>Logo</div>
+            <div>Home</div>
+            <div>Create</div>
+            <div>Sign in</div>
+        </header>
+        <main>
+            <div>Search bar</div>
+            <div>Search button</div>
+            <div>
+                <div>Thumbnail</div>
+                <div>Post Title</div>
+                <div>Category</div>
+                <div>Author Name</div>
+                <div>Elapsed TIme</div>
+                <div>Contents</div>
+            </div>
+            <div>Page Nation</div>
+        </main>
+    </div>
+  )
+}
