95cfa39bbc60ffcd2d19700f6817b9d471408f53
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2024-10-16 16:54:06 +0900

    homeコンポーネント修正

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,27 +1,24 @@
 import styles from "./page.module.css";
+import Header from "../components/Header";
+import Pagination from "../components/Pagination";
 
 export default function Home() {
   return (
-    <div>
-        <header>
-            <div>Logo</div>
-            <div>Home</div>
-            <div>Create</div>
-            <div>Sign in</div>
-        </header>
+    <>
+      <Header />
         <main>
-            <div>Search bar</div>
-            <div>Search button</div>
-            <div>
-                <div>Thumbnail</div>
-                <div>Post Title</div>
-                <div>Category</div>
-                <div>Author Name</div>
-                <div>Elapsed TIme</div>
-                <div>Contents</div>
-            </div>
-            <div>Page Nation</div>
+          <input type="text" />
+          <button>Search button</button>
+          <div>
+            <div>Thumbnail</div>
+            <div>Post Title</div>
+            <div>Category</div>
+            <div>Author Name</div>
+            <div>Elapsed TIme</div>
+            <div>Contents</div>
+          </div>
+          <Pagination />
         </main>
-    </div>
+    </>
   )
 }
