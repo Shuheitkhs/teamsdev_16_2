83ab7c16085419b13923317daff993a448eb4a99
Stk <shuheitakahashi0822@gmail.com>
2024-10-16 15:47:37 +0900

    🎨 formatとlintの確認

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import "./globals.css";
-import Pagination from "@/app/components/Pagination";
 
 export default function Home() {
   return (
     <>
       <p>Hello world!!</p>
-
     </>
   );
 }
