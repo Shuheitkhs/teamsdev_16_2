dcade212b5437c4ba7024704be48753a3b96434d
Stk <shuheitakahashi0822@gmail.com>
2024-10-17 12:53:25 +0900

    ✨ tailwindのカラーテーマを設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { Balance } = require("@mui/icons-material");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -6,7 +8,19 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //カラーでWF上の色を呼び出しやすく設定
+        blue: "#18A0FB",
+        black: "#383838",
+        red: "FF3131",
+        gray: "#C9C9C9",
+        white: "#FFFFFF",
+        // 必要なら便宜設定できるように用意
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
