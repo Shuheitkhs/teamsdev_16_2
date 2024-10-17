08c206c23cd2e41d0f6356bcbcbad12c37226939
Stk <shuheitakahashi0822@gmail.com>
2024-10-17 14:05:26 +0900

    🐛 tailwindのカラーテーマの修正

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
         //カラーでWF上の色を呼び出しやすく設定
         blue: "#18A0FB",
         black: "#383838",
-        red: "FF3131",
+        red: "#FF3131",
         gray: "#C9C9C9",
         white: "#FFFFFF",
         // 必要なら便宜設定できるように用意
