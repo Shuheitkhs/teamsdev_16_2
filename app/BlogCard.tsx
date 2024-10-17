0cd01b378b59cf230262b3f6bceea077631bdf32
Hisanori Ito <86972518+hisanori-ito@users.noreply.github.com>
2024-10-17 09:16:41 +0900

    lint, prettier対応

--- app/BlogCard.tsx
+++ app/BlogCard.tsx
@@ -1,23 +1,23 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function BlogCard() {
-    return (
-        <article>
-            <div>
-                <Image src="" alt="Profile" />
-            </div>
-            <div>
-                <h3>Post Title</h3>
-            </div>
-            <div>
-                <div>Category</div>
-            </div>
-            <div>
-                <div>Author</div>
-            </div>
-            <div>
-                <div>Time</div>
-            </div>
-        </article>
-    )
+  return (
+    <article>
+      <div>
+        <Image src="" alt="Profile" />
+      </div>
+      <div>
+        <h3>Post Title</h3>
+      </div>
+      <div>
+        <div>Category</div>
+      </div>
+      <div>
+        <div>Author</div>
+      </div>
+      <div>
+        <div>Time</div>
+      </div>
+    </article>
+  );
 }
