54a2f7b4f387d00af427a6f2475cca3d12b9e9c6
reoroi <reo0330210@gmail.com>
2024-11-08 17:55:27 +0900

    mainの最新状態へ更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Comment from "./components/Comment";
 import "./globals.css";
 
 export default function Home() {
   return (
     <>
       <p>Hello world!!</p>
+      <Comment />
     </>
   );
 }
