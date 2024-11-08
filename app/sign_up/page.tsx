a549d3d9bb23fff61f9b1e0042dd1df5804bf125
reoroi <reo0330210@gmail.com>
2024-11-08 21:07:15 +0900

    signupユーザ登録途中

--- app/sign_up/page.tsx
+++ app/sign_up/page.tsx
@@ -1,23 +1,60 @@
-import React from "react";
+"use client";
+import React, { ReactNode } from "react";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange" });
+
+  const clickSignUp = (data: any) => {
+    console.log("Submitted data:", data);
+  };
+
   return (
     <div>
       <h1>Sign Up</h1>
-      <form>
+      <form onSubmit={handleSubmit(clickSignUp)}>
         <div>
           <p>Name</p>
-          <input type="text" />
+          <input
+            type="text"
+            {...register("name", {
+              required: "Nameが入力されていません",
+              maxLength: {
+                value: 20,
+                message: "Nameは20文字以内としてください",
+              },
+            })}
+          />
+          <p className="text-red-500">{errors.name?.message as ReactNode}</p>
         </div>
         <div>
           <p>Email</p>
-          <input type="email" />
+          <input
+            type="email"
+            {...register("email", { required: "Emailが入力されていません" })}
+          />
+          <p className="text-red-500">{errors.email?.message as ReactNode}</p>
         </div>
         <div>
-          <p>PassWord</p>
-          <input type="password" />
+          <p>Password</p>
+          <input
+            type="password"
+            {...register("password", {
+              required: "パスワードを入力してください",
+              minLength: {
+                value: 8,
+                message: "パスワードは8文字以上にしてください",
+              },
+            })}
+            autoComplete="new-password"
+          />
+          <p className="text-red-500">{errors.password?.message as ReactNode}</p>
         </div>
-        <button>Sign Up</button>
+        <button type="submit">Sign Up</button>
       </form>
       <span>Already have an account?</span>
       <span> sign in</span>
