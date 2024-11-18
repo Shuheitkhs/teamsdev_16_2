943cc519e9d88473222e016f9a66abbfeb9c518c
reoroi <155074339+reoroi@users.noreply.github.com>
2024-11-18 20:30:22 +0900

    SignUp画面でのユーザ登録処理成功 (#77)
    * write_viewのコンポーネント作成
    
    * signupユーザ登録途中
    
    * signUpでのユーザ登録処理完成
    
    * signIn画面へ遷移処理追加
    
    * 指摘箇所修正
    
    * supabseの定義ファイルのインポート先変更

--- app/sign_up/page.tsx
+++ app/sign_up/page.tsx
@@ -1,29 +1,54 @@
 "use client";
-import React, { ReactNode } from "react";
+import { SignUpData } from "@/Types";
+import React, { ReactNode, useState } from "react";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import supabase from "@/lib/Supabase/Client";
 
 const SignUp = () => {
+  const router = useRouter();
   //useFormを使ってバリデーションのチェック
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: "onChange" });
+  } = useForm<SignUpData>({ mode: "onChange" });
+  const [signUpError, setSignUpError] = useState("");
 
-  const clickSignUp = (data: any) => {
-    console.log(data);
+  const clickSignUp = async (signUpData: SignUpData) => {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: signUpData.email,
+        password: signUpData.password,
+      });
+      console.log(data, "ユーザ登録に成功しました");
+      if (error) {
+        throw error;
+      }
+    } catch (error: any) {
+      if (error.message == "User already registered") {
+        setSignUpError("すでに登録されているユーザです");
+        return;
+      } else {
+        console.log("すでに登録はされていません");
+      }
+      console.log(error, "ユーザ登録に失敗しました");
+      return;
+    }
+    router.push("/sign_in");
   };
 
   return (
     <div>
       <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit(clickSignUp)}>
+      <form onSubmit={handleSubmit((signUpData) => clickSignUp(signUpData))}>
+        <p className="text-red-500">{signUpError}</p>
         <div>
           <p>Name</p>
           {/* Nameのバリデーションをチェックしエラーメッセージを表示 */}
           <input
             type="text"
-            {...register("Name", {
+            {...register("name", {
               required: "Nameが入力されていません",
               maxLength: {
                 value: 30,
@@ -31,15 +56,14 @@ const SignUp = () => {
               },
             })}
           />
-          <p className="text-red-500">{errors.Name?.message as ReactNode}</p>
+          <p className="text-red-500">{errors.name?.message as ReactNode}</p>
         </div>
         <div>
           <p>Email</p>
           {/* Nameがバリデーションをチェックしエラーメッセージを表示 */}
-
           <input
             type="email"
-            {...register("Email", {
+            {...register("email", {
               required: "Emailが入力されていません",
               pattern: {
                 value:
@@ -48,14 +72,14 @@ const SignUp = () => {
               },
             })}
           />
-          <p className="text-red-500">{errors.Email?.message as ReactNode}</p>
+          <p className="text-red-500">{errors.email?.message as ReactNode}</p>
         </div>
         <div>
           <p>PassWord</p>
           {/* Passwordのバリデーションをチェックしエラーメッセージを表示 */}
           <input
             type="password"
-            {...register("Password", {
+            {...register("password", {
               required: "パスワードを入力してください",
               minLength: {
                 value: 8,
@@ -64,7 +88,7 @@ const SignUp = () => {
             })}
           />
           <p className="text-red-500">
-            {errors.Password?.message as ReactNode}
+            {errors.password?.message as ReactNode}
           </p>
         </div>
         <button>Sign Up</button>
